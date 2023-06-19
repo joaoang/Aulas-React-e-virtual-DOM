8d03f4349d082ee00a17a0c1329b9ede61f2d061
João Angrezani <jvpb1234@hotmail.com>
2023-06-19 03:42:54 -0300

    Primeiro Upload
    Montei a área de trabalho.

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+function App() {
+return (
+        <section className='App'>
+            <h1>Contador</h1>
+            <div>
+                <div>0</div>
+                <button>Incrementar</button>
+                <button>Decrementar</button>
+            </div>
+        </section>
+    );
+}
+
+render(<App />, document.querySelector('#root'));
