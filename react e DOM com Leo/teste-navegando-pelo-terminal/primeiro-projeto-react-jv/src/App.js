c60532acf9560df26c7c97feed51176b45d1a355
João Angrezani <jvpb1234@hotmail.com>
2023-06-19 18:06:22 -0300

    função show

--- react e DOM com Leo/teste-navegando-pelo-terminal/primeiro-projeto-react-jv/src/App.js
+++ react e DOM com Leo/teste-navegando-pelo-terminal/primeiro-projeto-react-jv/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import Item from './item'
 
 function App() {
+  function consoleMaroto(){
+    console.log("Foi!")
+  }
+
   return (
     <div>
-      <Item/>
-      <Item/>
-      <Item/>
+      <Item titulo="Chupa cabra aterroriza cidade" sub="ha ha ha" funçaoShow ={consoleMaroto}/>
+      <br/>
+      <Item titulo="João Angrezani foi chamado para trabalhar em uma empresa DEV."/>
     </div>
+    
   );
 }
 
--- react e DOM com Leo/teste-navegando-pelo-terminal/primeiro-projeto-react-jv/src/Item.jsx
+++ react e DOM com Leo/teste-navegando-pelo-terminal/primeiro-projeto-react-jv/src/Item.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export default function item() {
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function item(props) {
   return (
-    <div>
-      <h1>Notícias</h1>
-      <h2>Babado Forte</h2>
-      <p>Blá blá blá</p>
-    </div>
-  )
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {props.sub}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props.titulo}
+        </Typography>
+        <Typography variant="body2">
+          Blá blá bla
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={props.funçaoShow}>Botão</Button>
+      </CardActions>
+    </Card>
+  );
 }
