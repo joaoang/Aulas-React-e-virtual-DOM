cd643497ff6985e72aac61e1a98b48b6bb6baa4e
João Angrezani <jvpb1234@hotmail.com>
2023-06-19 07:35:53 -0300

    Conhecendo a plataforma

---
+++ react e DOM com Leo/teste-navegando-pelo-terminal/primeiro-projeto-react-jv/src/Item.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function item() {
+  return (
+    <div>
+      <h1>Notícias</h1>
+      <h2>Babado forte</h2>
+      <p>Blá blá blá</p>
+    </div>
+  )
+}
